de03a2a25045e50b6d9d7232129dec155e47fd18
chs4649 <chs4649@gmail.com>
2025-04-07 11:48:17 +0900

    @ResponseBody

--- src/main/java/com/project/sbb/controller/MainController.java
+++ src/main/java/com/project/sbb/controller/MainController.java
@@ -2,12 +2,14 @@ package com.project.sbb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class MainController {
  @RequestMapping("/sbb")
- public void index() {
-    System.out.println("start");
+ @ResponseBody
+ public String index() {
+   return "Hello";
   }
 }
