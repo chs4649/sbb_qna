319ab8e24361d12dcfe7ff15a5766b15f0157ca7
chs4649 <chs4649@gmail.com>
2025-04-07 11:40:25 +0900

    MainController::add index

---
+++ src/main/java/com/project/sbb/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.project.sbb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MainController {
+ @RequestMapping("/sbb")
+ public void index() {
+    System.out.println("start");
+  }
+}
